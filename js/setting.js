67f8003cc0f8aa05d245748dd282ff72af650d2e
jumpei79 <jumpei79@gmail.com>
2015-05-09 15:53:30 +0900

    setting テスト

--- js/setting.js
+++ js/setting.js
@@ -19,17 +19,20 @@ OpenSpending.Styles.Cofog = {
 
   /* toshima cofog */
   '1': { icon: 'icons/government-uk.svg', color: '#38a1db', bcolor: '#935B3B' },
+  '201301': { icon: 'icons/government-uk.svg', color: '#38a1db', bcolor: '#935B3B' },
   '2': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '3': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '4': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '5': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '6': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '7': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-  '2': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
-
-
-
+  '201302': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201303': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201304': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201305': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201306': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201307': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201308': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
+  '201309': { icon: 'icons/yokohama/publicaffairs.svg', color: '#38a1db', bcolor: '#0AB971' },
   '3': { icon: 'icons/helping-others.svg', color: '#38a1db', bcolor: '#4E6D00' },
+  '201310': { icon: 'icons/helping-others.svg', color: '#38a1db', bcolor: '#4E6D00' },
+  '201311': { icon: 'icons/wheelchair.svg', color: '#38a1db', bcolor: '#4E6D00' },
+  '201312': { icon: 'icons/old-age.svg', color: '#38a1db', bcolor: '#4E6D00' },
   '4': { icon: 'icons/ambulance.svg', color: '#38a1db', bcolor: '#0AB971' },
   '5': { icon: 'icons/waste.svg', color: '#38a1db', bcolor: '#0AB971' },
   '6': { icon: 'icons/housing.svg', color: '#38a1db', bcolor: '#0AB971' },
